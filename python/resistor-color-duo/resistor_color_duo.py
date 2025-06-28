2caa15ff1133ca6faab31704984e4879e9bd0fd2
Arunimavpa <arunimaajithvp14@gmail.com>
2025-06-28 21:28:07 +0530

    add resistor-color-duo

---
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,7 @@
+def value(colors):
+    color=["black","brown","red","orange","yellow","green","blue","violet","grey","white"]
+    index1=color.index(colors[0])
+    index2=color.index(colors[1])
+    return index1*10 + index2
+
+
